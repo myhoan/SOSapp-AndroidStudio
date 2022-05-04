704cdab4231146d2a1e72c126eab673a7ee97026
myhoan <18520771@gm.uit.edu.vn>
2022-05-04 20:45:57 +0700

    add file SensorService.java, ReactivateService.java, ShakeDetector.java

---
+++ app/src/main/java/com/finalproject/sosapp/ShakeDetector.java
@@ -0,0 +1,66 @@
+package com.finalproject.sosapp;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.view.KeyEvent;
+
+public class ShakeDetector implements SensorEventListener {
+
+    private static final float SHAKE_THRESHOLD_GRAVITY = 2.7F;
+    private static final int SHAKE_SLOP_TIME_MS = 500;
+    private static final int SHAKE_COUNT_RESET_TIME_MS = 3000;
+
+    private OnShakeListener mListener;
+    private long mShakeTimestamp;
+    private int mShakeCount;
+
+    public void setOnShakeListener(OnShakeListener listener) {
+        this.mListener = listener;
+    }
+
+    public interface OnShakeListener {
+        public void onShake(int count);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // ignore
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (mListener != null) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            float gX = x / SensorManager.GRAVITY_EARTH;
+            float gY = y / SensorManager.GRAVITY_EARTH;
+            float gZ = z / SensorManager.GRAVITY_EARTH;
+
+            Float f = new Float(gX * gX + gY * gY + gZ * gZ);
+            Double d = Math.sqrt(f.doubleValue());
+            float gForce = d.floatValue();
+
+            if (gForce > SHAKE_THRESHOLD_GRAVITY) {
+                final long now = System.currentTimeMillis();
+                if (mShakeTimestamp + SHAKE_SLOP_TIME_MS > now) {
+                    return;
+                }
+
+                if (mShakeTimestamp + SHAKE_COUNT_RESET_TIME_MS < now) {
+                    mShakeCount = 0;
+                }
+
+                mShakeTimestamp = now;
+                mShakeCount++;
+
+                mListener.onShake(mShakeCount);
+            }
+        }
+    }
+
+}
